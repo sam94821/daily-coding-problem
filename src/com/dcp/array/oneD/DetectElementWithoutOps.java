e9bc688063662a98cd1a026008d75fbc7b967de7
Sam <87396031+sam94821@users.noreply.github.com>
2021-08-14 18:01:15 +0530

    CheckElementInArray + TestCase

--- src/com/dcp/array/oneD/DetectElementWithoutOps.java
+++ src/com/dcp/array/oneD/DetectElementWithoutOps.java
@@ -13,7 +13,7 @@ package com.dcp.array.oneD;
  *  --------------------------------------------------------------------------------
  */
 public class DetectElementWithoutOps {
-	
+
 	private static int element;
 	private static int[] sortedArray;
 	private static boolean elementIsPresent;
